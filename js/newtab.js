25e9a74c06d189e243395e282922957a52ed8076
dotRamsey <dotramsey87@gmail.com>
2021-04-09 20:54:00 +0200

    Fixes
    There are 365 days in a year not 356. and then there is this leap year kinda thing..

--- js/newtab.js
+++ js/newtab.js
@@ -5,7 +5,7 @@ function renderAge () {
         var current_date = new Date(); // Date time of now
         var birthday = new Date('July 17, 1987 09:10:00'); // date time of when i was born
         var diffTime = current_date - birthday; // The difference in milliseconds
-        var years = ( diffTime / (1000 * 60 * 60 * 24 * 356) ) - 1; // The difference in years
+        var years = diffTime / (1000 * 60 * 60 * 24 * 365.25); // The difference in years
         span.textContent = years.toFixed(9);
     }
 
